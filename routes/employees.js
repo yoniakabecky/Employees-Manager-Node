1ce21508ec454d7cb624ecbde11cff8915075933
yoniakabecky <yoniakabecky@gmail.com>
2019-09-15 20:28:52 -0700

    change access control thing

--- routes/employees.js
+++ routes/employees.js
@@ -6,9 +6,9 @@ let Employees = require('../models/employees.mongo');
 
 router.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader("Access-Control-Allow-Credentials", "true");
-  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  // response.setHeader("Access-Control-Allow-Credentials", "true");
+  // response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  // response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
